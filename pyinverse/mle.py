147768be5e1a5bcf0dbdb6b59de77d18b1b27fa8
Roman Rodionov <rodionov.rn@yandex.ru>
2020-09-27 22:42:02 +0300

    Add removing of zero rows and cols of proj

--- pyinverse/mle.py
+++ pyinverse/mle.py
@@ -40,26 +40,32 @@ def maximize_likelihood_EM(y, x0, proj, max_iter=100, abstol=1.e-8, smooth=None)
     if smooth.shape != (N, N):
         raise ValueError('Inconsistent shape of smooth operator: {0}'.format(smooth.shape))
 
+    index_row, index_col = get_nonzero_indices(proj)
+
+    proj = proj[index_row, :][:, index_col]
+    y = y[index_row]
+
     S = 1.0 / np.sum(proj, axis=0)
 
-    sum_log_k = np.sum([sum_of_factorial_logs(yi) for yi in y])
+    sum_log_k = np.sum([factorial_log(yi) for yi in y])
 
     x_old = x0
     iter = 0
 
     while True:
-        denom = np.dot(proj, x_old)
+        denom = np.dot(proj, x_old[index_col])
         coeff = np.dot(y / denom, proj)
-        x_new = x_old * S * coeff
+        x_new = x_old.copy()
+        x_new[index_col] = x_old[index_col] * S * coeff
         
         x_new = np.dot(smooth, x_new)
 
-        lam = np.dot(proj, x_new)
+        lam = np.dot(proj, x_new[index_col])
         logp = np.dot(y, np.log(lam)) - np.sum(lam) - sum_log_k
 
         if iter == max_iter:
             break
-        if np.linalg.norm(y - np.dot(proj, x_new)) < abstol:
+        if np.linalg.norm(y - np.dot(proj, x_new[index_col])) < abstol:
             break
 
         x_old = x_new
@@ -67,8 +73,28 @@ def maximize_likelihood_EM(y, x0, proj, max_iter=100, abstol=1.e-8, smooth=None)
     return x_new, logp
 
 
-def sum_of_factorial_logs(k):
+def factorial_log(k):
+    """Calculates log(k!)."""
     result = 0
     for i in range(1, k+1):
         result += np.log(i)
     return result
+
+
+def get_nonzero_indices(proj):
+    """Finds nonzero rows and columns of projection matrix.
+
+    Parameters
+    ----------
+    proj : array_like
+        Projection matrix.
+    
+    Returns
+    -------
+    index_row, index_column : array[bool]
+        Row and column index arrays.
+    """
+    index_row = np.array(np.count_nonzero(proj, axis=1), dtype=bool)
+    index_col = np.array(np.count_nonzero(proj, axis=0), dtype=bool)
+    return index_row, index_col
+
